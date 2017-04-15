7aa64f8dfc1f646bfd608d9165ad945eb2b9be1c
Lucas Bebber <lucasbbebber@gmail.com>
2017-04-15 19:16:02 -0300

    dev

--- index.test.js
+++ index.test.js
@@ -2,11 +2,5 @@ import {add} from './index';
 
 test('Add',()=>{
   expect(add(1,2)).toBe(3);
-});
-
-test('Delayed add',done=>{
-  setTimeout(()=>{
-    expect(add(1,2)).toBe(3);
-    done();
-  },1000);
+  expect(add(0,0)).toBe(0);
 });
